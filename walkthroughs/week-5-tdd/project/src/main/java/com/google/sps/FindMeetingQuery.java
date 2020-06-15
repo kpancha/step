1d7903bafb5028a8f237433ea98f5edd4540b5ee
Kira Pancha <kirakpancha@gmail.com>
2020-06-15 14:55:32 +0000

    Implement query function.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,74 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    Set<TimeRange> busyTimesSet = new HashSet<>();
+    for (Event event : events) {
+      if (hasCommonAttendees(event.getAttendees(), request.getAttendees())) {
+        TimeRange eventTime = event.getWhen();
+        boolean added = addToBusySet(busyTimesSet, eventTime);
+        if (!added) {
+          busyTimesSet.add(eventTime);
+        }
+      }
+    }
+    return getFreeTimesFromBusySet(busyTimesSet, request.getDuration());
+  }
+
+  private boolean hasCommonAttendees(Collection<String> eventAttendees, Collection<String> requestAttendees) {
+    return !Collections.disjoint(eventAttendees, requestAttendees);
+  }
+
+  private boolean addToBusySet(Set<TimeRange> busyTimesSet, TimeRange eventTime) {
+    Iterator iterator = busyTimesSet.iterator();
+    while (iterator.hasNext()) {
+      TimeRange currRange = (TimeRange) iterator.next();
+      if (currRange.contains(eventTime)) {
+        return true;
+      } else if (eventTime.contains(currRange)) {
+        busyTimesSet.remove(currRange);
+        busyTimesSet.add(eventTime);
+        return true;
+      } else if (currRange.overlaps(eventTime)) {
+        if (currRange.contains(eventTime.start())) {
+          busyTimesSet.remove(currRange);
+          busyTimesSet.add(TimeRange.fromStartEnd(currRange.start(), eventTime.end(), false));
+        } else {
+          busyTimesSet.remove(currRange);
+          busyTimesSet.add(TimeRange.fromStartEnd(eventTime.start(), currRange.end(), true));
+        }
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private Collection<TimeRange> getFreeTimesFromBusySet(Set<TimeRange> busyTimesSet, long duration) {
+    List<TimeRange> busyTimesList = new ArrayList<>(busyTimesSet);
+    Collections.sort(busyTimesList, TimeRange.ORDER_BY_START);
+    Collection<TimeRange> freeTimes = new ArrayList<>();
+    int startTime = TimeRange.START_OF_DAY;
+    int endTime;
+    for (TimeRange busyTime : busyTimesList) {
+      if (busyTime.start() - startTime >= duration) {
+        endTime = busyTime.start();
+        freeTimes.add(TimeRange.fromStartEnd(startTime, endTime, false));
+      }
+      startTime = busyTime.end();
+    }
+    if (TimeRange.END_OF_DAY - startTime >= duration) {
+      endTime = TimeRange.END_OF_DAY;
+      freeTimes.add(TimeRange.fromStartEnd(startTime, endTime, true));
+    }
+    return freeTimes;
   }
 }
