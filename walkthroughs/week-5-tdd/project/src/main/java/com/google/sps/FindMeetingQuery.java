998b25b941a16e4e605e013230edae9e7295062f
Kira Pancha <kirakpancha@gmail.com>
2020-06-15 20:50:10 +0000

    Address comments(add comments, param tags, new var names).

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -34,14 +34,16 @@ public final class FindMeetingQuery {
         }
       }
     }
-    return getFreeTimesFromBusySet(busyTimesSet, request.getDuration());
+    return findFreeTimes(busyTimesSet, request.getDuration());
   }
 
-  private boolean hasCommonAttendees(Collection<String> eventAttendees, Collection<String> requestAttendees) {
+  private static boolean hasCommonAttendees(Collection<String> eventAttendees, Collection<String> requestAttendees) {
     return !Collections.disjoint(eventAttendees, requestAttendees);
   }
 
-  private boolean addToBusySet(Set<TimeRange> busyTimesSet, TimeRange eventTime) {
+  // Merges a new time range with existing time range in set, if necessary.
+  // Returns whether or not the time range was merged.
+  private static boolean addToBusySet(Set<TimeRange> busyTimesSet, TimeRange eventTime) {
     Iterator iterator = busyTimesSet.iterator();
     while (iterator.hasNext()) {
       TimeRange currRange = (TimeRange) iterator.next();
@@ -54,10 +56,10 @@ public final class FindMeetingQuery {
       } else if (currRange.overlaps(eventTime)) {
         if (currRange.contains(eventTime.start())) {
           busyTimesSet.remove(currRange);
-          busyTimesSet.add(TimeRange.fromStartEnd(currRange.start(), eventTime.end(), false));
+          busyTimesSet.add(TimeRange.fromStartEnd(currRange.start(), eventTime.end(), /* inclusive= */ false));
         } else {
           busyTimesSet.remove(currRange);
-          busyTimesSet.add(TimeRange.fromStartEnd(eventTime.start(), currRange.end(), true));
+          busyTimesSet.add(TimeRange.fromStartEnd(eventTime.start(), currRange.end(), /* inclusive= */ true));
         }
         return true;
       }
@@ -65,7 +67,9 @@ public final class FindMeetingQuery {
     return false;
   }
 
-  private Collection<TimeRange> getFreeTimesFromBusySet(Set<TimeRange> busyTimesSet, long duration) {
+  // Takes in a set of busy time ranges to find all free time ranges based on duration of the meeting.
+  // Returns all possibilities as a collection.
+  private static Collection<TimeRange> findFreeTimes(Set<TimeRange> busyTimesSet, long duration) {
     List<TimeRange> busyTimesList = new ArrayList<>(busyTimesSet);
     Collections.sort(busyTimesList, TimeRange.ORDER_BY_START);
     Collection<TimeRange> freeTimes = new ArrayList<>();
@@ -74,13 +78,13 @@ public final class FindMeetingQuery {
     for (TimeRange busyTime : busyTimesList) {
       if (busyTime.start() - startTime >= duration) {
         endTime = busyTime.start();
-        freeTimes.add(TimeRange.fromStartEnd(startTime, endTime, false));
+        freeTimes.add(TimeRange.fromStartEnd(startTime, endTime, /* inclusive= */ false));
       }
       startTime = busyTime.end();
     }
     if (TimeRange.END_OF_DAY - startTime >= duration) {
       endTime = TimeRange.END_OF_DAY;
-      freeTimes.add(TimeRange.fromStartEnd(startTime, endTime, true));
+      freeTimes.add(TimeRange.fromStartEnd(startTime, endTime, /* inclusive= */ true));
     }
     return freeTimes;
   }
